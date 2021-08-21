7a4602fdc62435f472546fe367d162d52f32c4b5
bytego20 <86067274+bytego20@users.noreply.github.com>
2021-08-21 21:19:04 +0800

    update new ui

---
+++ js/about.03d85a97.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var l=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about",staticStyle:{height:"100vh"}},[n("h1",[t._v("Join Us And Build A Better World!")])])}],a=n("2877"),i={},r=Object(a["a"])(i,l,s,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.03d85a97.js.map
